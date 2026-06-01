20c65318303dcd3e22cd5ded397ce96ccd8caae6
om5957050-jpg <om5957050@gmail.com>
2026-06-01 15:02:19 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+public class oddeven {
+    public static void main(String[] args) {
+       for (int num=1; num <=20; num++) {
+        if (num%2==0) {
+            System.out.println(num + " is an even number");
+        }
+        else{
+            System.out.println(num + " is an odd number");
+        }
+    }
+}
+}
