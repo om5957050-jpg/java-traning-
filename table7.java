5a30468e6ffe9f8edec9f39fc2f7ebbddf994617
om5957050-jpg <om5957050@gmail.com>
2026-06-01 14:47:27 +0530

    Add files via upload

---
+++ table7.java
@@ -0,0 +1,7 @@
+public class table7 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++) {
+            System.out.println("7 x " + i + " = " + (7*i));
+        }
+    }
+}
